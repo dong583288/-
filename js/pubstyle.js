3f5c1ec4170e836912b74e410972992e493cd721
dong583288 <dong583288@foxmail.com>
2018-12-13 18:46:43 +0800

    first

---
+++ js/pubstyle.js
@@ -0,0 +1,5 @@
+// document.write("<script language=javascript src='./js/flexible_css.js'></script>");
+// document.write("<script language=javascript src='./js/flexible.js'></script>");
+document.write("<link href='./style/css/mui.min.css' rel='stylesheet'/>");
+document.write("<link href='./style/css/iconfont.css' rel='stylesheet'/>");
+
